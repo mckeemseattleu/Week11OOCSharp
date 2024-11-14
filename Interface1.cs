8da409f00b47f6a55da5da057e57284221eb0114
mckeemseattleu <mckeem@seattleu.edu>
2024-11-14 13:42:58 -0800

    Commit changes

--- Interface1.cs
+++ Interface1.cs
@@ -1,70 +1,75 @@
 ﻿//https://www.geeksforgeeks.org/c-sharp-inheritance-in-interfaces/
 namespace Week8OOCSharp;
 using System;
-public class Interface1
-{
+
 // C# program to illustrate the concept
 // of inheritance in the interface
 
+// Good breakout -
+// 1. To create another Subtype of Votes
+// 2. Add data to subtype
+// 3. 
+// 3. To add a function to be implemented that will
+//    throw exception when it can't be implemented
 
 // declaring an interface
-    public interface Votes 
+public interface Votes 
+{
+    
+    // method of interface 
+    void vote_no(int a);
+}
+
+// The methods of interface Votes
+// is inherited into interface Details
+public interface Details : Votes 
+{
+    
+    // method of interface Details
+    void detailsofauthor(string n, int p);
+    
+}
+
+// Below class is inheriting 
+// the interface Details
+// This class must implement 
+// the methods of both interface
+// i.e. Votes and Details
+class TechnicalScriptWriter : Details 
+{
+
+    // implementing the method 
+    // of interface Votes
+    public void vote_no(int a)
     {
-        
-        // method of interface 
-        void vote_no(int a);
+        Console.WriteLine("Total number of votes is: {0}", a);
     }
-
-    // The methods of interface Votes
-    // is inherited into interface Details
-    public interface Details : Votes 
+    
+    // implementing the method 
+    // of interface Details 
+    public void detailsofauthor(string n, int p)
     {
+        Console.WriteLine("Name of the author is: {0}", n);
         
-        // method of interface Details
-        void detailsofauthor(string n, int p);
+        Console.WriteLine("Total number of published" + 
+                          " article is: {0}", p);
     }
+}
 
-    // Below class is inheriting 
-    // the interface Details
-    // This class must implement 
-    // the methods of both interface
-    // i.e. Votes and Details
-    class TechnicalScriptWriter : Details 
+// Driver Class
+class GFG {
+
+    // Main method
+    static void Main7()
     {
 
-        // implementing the method 
-        // of interface Votes
-        public void vote_no(int a)
-        {
-            Console.WriteLine("Total number of votes is: {0}", a);
-        }
+        // Creating the objects of 
+        // TechinalScriptWriter class
+        TechnicalScriptWriter obj = new TechnicalScriptWriter();
         
-        // implementing the method 
-        // of interface Details 
-        public void detailsofauthor(string n, int p)
-        {
-            Console.WriteLine("Name of the author is: {0}", n);
-            
-            Console.WriteLine("Total number of published" + 
-                              " article is: {0}", p);
-        }
-    }
-
-    // Driver Class
-    class GFG {
-
-        // Main method
-        static void Main7()
-        {
-
-            // Creating the objects of 
-            // TechinalScriptWriter class
-            TechnicalScriptWriter obj = new TechnicalScriptWriter();
-            
-            // calling the methods by passing
-            // the required values
-            obj.vote_no(470045);
-            obj.detailsofauthor("Siya", 98);
-        }
+        // calling the methods by passing
+        // the required values
+        obj.vote_no(470045);
+        obj.detailsofauthor("Siya", 98);
     }
 }
---
+++ Penguin.cs
@@ -0,0 +1,69 @@
+﻿namespace Week8OOCSharp;
+
+public class Penguin_liskov
+{
+    public class Bird
+    {
+        public string Name { get; }
+
+        public Bird(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine($"{Name} eats.");
+        }
+
+        public virtual void LayEggs()
+        {
+            Console.WriteLine($"{Name} lays eggs.");
+        }
+
+        public virtual void Fly()
+        {
+            Console.WriteLine($"{Name} can fly.");
+        }
+
+    }
+
+    public class Duck : Bird
+    {
+        public Duck(string name) : base(name)
+        {
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{Name} can fly.");
+        }
+    }
+
+    public class Kiwi : Bird
+    {
+        public Kiwi(string name) : base(name)
+        {
+        }
+    }
+
+    public class Penguin : Bird
+    {
+        public Penguin(string name) : base(name)
+        {
+        }
+
+        public override void Fly()
+        {
+            Console.WriteLine($"{Name} cant fly.");
+        }
+    }
+
+    int main_penguin()
+    {
+        // Try to make a Penguin Fly
+        Bird birdObj = new Penguin("Pengy");
+        birdObj.Fly();
+        return 0;
+    }
+}
