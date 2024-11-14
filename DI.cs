ff7c032bc8c4f669c866105a5fedf7c1cf47c916
mckeemseattleu <mckeem@seattleu.edu>
2024-11-14 07:41:42 -0800

    Initial commit

---
+++ DI.cs
@@ -0,0 +1,45 @@
+﻿namespace Week8OOCSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+//https://www.c-sharpcorner.com/UploadFile/85ed7a/dependency-injection-in-C-Sharp/
+
+public class DI
+{
+    public interface text
+    {
+        void print();
+    }
+    class format : text
+    {
+        public void print()
+        {
+            Console.WriteLine(" here is text format");
+        }
+    }
+    // constructor injection
+    public class constructorinjection
+    {
+        private text _text;
+        public constructorinjection(text t1)
+        {
+            this._text = t1;
+        }
+        public void print()
+        {
+            _text.print();
+        }
+    }
+    class constructor
+    {
+        static void Main_DI(string[] args)
+        {
+            constructorinjection cs = new constructorinjection(new format());
+            cs.print();
+            Console.ReadKey();
+        }
+    }
+ 
+}
---
+++ Week8OO.cs
@@ -0,0 +1,55 @@
+﻿public class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a shapeshape");
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle");
+    }
+}
+
+public class Triangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a triangle");
+    }
+}
+
+class ShapeFactory
+{
+    public static Shape CreateShape()
+    {
+        return new Triangle();
+    }
+}
+class Program1 {
+    public static void Main_poly (string[] args) {
+        Console.WriteLine("This is a shape factory");
+        Shape shape1 = new Shape();
+        Shape shape2 = new Circle();
+        Shape shape3 = new Rectangle();
+        Shape shape4 = new Triangle();
+        Shape shape5 = ShapeFactory.CreateShape();
+
+        shape1.Draw(); // Outputs "Drawing a shape"
+        shape2.Draw(); // Outputs "Drawing a circle"
+        shape3.Draw(); // Outputs "Drawing a rectangle"
+        shape4.Draw(); // Outputs "Drawing a triangle"     
+        shape5.Draw();
+    }
+}
